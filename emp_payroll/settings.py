358e7fed58a286e12830edb2cbaa964622ba04ff
EC2 Default User <ec2-user@ip-172-31-4-227.eu-west-1.compute.internal>
2025-03-21 12:45:32 +0000

    pushing awsutils

--- emp_payroll/settings.py
+++ emp_payroll/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-7f*28e(^#gpb(=!=i83w9f^-)hab62d%^77#avvbhi%x=hh(y8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.250.52.149']
+ALLOWED_HOSTS = ['3.250.52.149',
+    '671dd204b08444d990fc638003860ff1.vfs.cloud9.eu-west-1.amazonaws.com']
 
 
 # Application definition
--- payroll/aws_utils.py
+++ payroll/aws_utils.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from datetime import datetime
 
 # 🔹 Replace with actual AWS values from Learner Lab
-AWS_ACCESS_KEY = "ASIATUYJP7SUJQB7GVU6"
+"""AWS_ACCESS_KEY = "ASIATUYJP7SUJQB7GVU6"
 AWS_SECRET_KEY = "i18XBOmV8B071uc8UgRMeVIbiWUl3NZOiapq8Nfz"
-AWS_SESSION_TOKEN = "IQoJb3JpZ2luX2VjEEwaCXVzLWVhc3QtMSJIMEYCIQDSGvFy88gDT+L9es23/fz2vQmvrP23PaGFiSGPMModPwIhAMpsCsM369qd++5iTWLPEh58Q+Miy48msJxqI12VdfMWKokECKX//////////wEQAxoMMjUwNzM4NjM3OTkyIgzW2Egkqo7Vk4yxnN8q3QNXpg021Sn6pzUJZmwgoMFU0s2T14ikxxr0VAXaCY/MYzlwdnnaLlbcuIq03pIJs8Hm0pz4bJPxYBf4tfLzG0aB2vXLXG0xpbAeZMA6nlfYjSQTWDiTqld1OTl/DLWtLMFa/jLky8q7o9uv9bgG/kTQwn27LBhEhwLGi7Y/HifbidOTDngVBEpOB0QYiT4/OFEiI2RetCJre67cedDmwUrnJ5LrqLSbTqE0H3PvB9p65TsEGmq3D74ewp1Ikj32sLeiwAu9ifMowv/H1E5XjRBf+dtXNls3eiXYFjnoD276gSF0/t8FnzdNM/OLuGG6zAmYzVDqTMlzrJqt0dzJ/N3V5yx9e/5rLOiyKXpZcEL2SzHwIyV0nJ04zVOBunuc0BwQo5o7NflJgTFXLAu+/MKWtUEyPzBS9yZcN/705e+NH9U+Q+mnCmMAgAm2DZobLu6a0zcVFnbZ7r+GtgLc5Ga3pmobOdTgG6DESo6e0ZMeIGgZbF3dX4G3uECKAVQWyy5ex+So5W1ZORJl0ZyEJCLmdjoidH/GlLRX7nSDJ1X5M0vZ9iZ5nOheku1cNl9cGYUjyHAoYJhnh0wyl4r3Boxn4tWe3DW/OOmFIhpxkfDCVTeyWux5KZwzprpDKwMw8J31vgY6pQFRnoh/Nw0R0aNyjgdAhgdzz0vr726vr0F0IJmJ3CDdcBXyZba63TTxQiuvzAY0c/qA5w+LK8KSNBHbznzQHnaER9gDxlidE8WNVOjckiIduK1w4dMr8wU2v137emeKLPzScdOF/IiNmPKjj2T/9myd+N3MfjvORjfWS0eEP76V78Y5VoJmI+m+G6hCZWspCjb0IGBnTSmPcFpS+Up/WoL7m9mx7kA="  # Temporary session token
+AWS_SESSION_TOKEN = "IQoJb3JpZ2luX2VjEEwaCXVzLWVhc3QtMSJIMEYCIQDSGvFy88gDT+L9es23/fz2vQmvrP23PaGFiSGPMModPwIhAMpsCsM369qd++5iTWLPEh58Q+Miy48msJxqI12VdfMWKokECKX//////////wEQAxoMMjUwNzM4NjM3OTkyIgzW2Egkqo7Vk4yxnN8q3QNXpg021Sn6pzUJZmwgoMFU0s2T14ikxxr0VAXaCY/MYzlwdnnaLlbcuIq03pIJs8Hm0pz4bJPxYBf4tfLzG0aB2vXLXG0xpbAeZMA6nlfYjSQTWDiTqld1OTl/DLWtLMFa/jLky8q7o9uv9bgG/kTQwn27LBhEhwLGi7Y/HifbidOTDngVBEpOB0QYiT4/OFEiI2RetCJre67cedDmwUrnJ5LrqLSbTqE0H3PvB9p65TsEGmq3D74ewp1Ikj32sLeiwAu9ifMowv/H1E5XjRBf+dtXNls3eiXYFjnoD276gSF0/t8FnzdNM/OLuGG6zAmYzVDqTMlzrJqt0dzJ/N3V5yx9e/5rLOiyKXpZcEL2SzHwIyV0nJ04zVOBunuc0BwQo5o7NflJgTFXLAu+/MKWtUEyPzBS9yZcN/705e+NH9U+Q+mnCmMAgAm2DZobLu6a0zcVFnbZ7r+GtgLc5Ga3pmobOdTgG6DESo6e0ZMeIGgZbF3dX4G3uECKAVQWyy5ex+So5W1ZORJl0ZyEJCLmdjoidH/GlLRX7nSDJ1X5M0vZ9iZ5nOheku1cNl9cGYUjyHAoYJhnh0wyl4r3Boxn4tWe3DW/OOmFIhpxkfDCVTeyWux5KZwzprpDKwMw8J31vgY6pQFRnoh/Nw0R0aNyjgdAhgdzz0vr726vr0F0IJmJ3CDdcBXyZba63TTxQiuvzAY0c/qA5w+LK8KSNBHbznzQHnaER9gDxlidE8WNVOjckiIduK1w4dMr8wU2v137emeKLPzScdOF/IiNmPKjj2T/9myd+N3MfjvORjfWS0eEP76V78Y5VoJmI+m+G6hCZWspCjb0IGBnTSmPcFpS+Up/WoL7m9mx7kA=" """ # Temporary session token
 
 AWS_S3_BUCKET = "x24104558cppbucket"
 AWS_SNS_TOPIC_ARN = "arn:aws:sns:eu-west-1:250738637992:EmployeeUpdates"
@@ -14,7 +14,7 @@ AWS_CLOUDWATCH_LOG_GROUP = "EmployeeActivityLogs"
 AWS_CLOUDWATCH_LOG_STREAM = "GeneralLogs"
 
 # 🔹 Configure AWS Clients with manual credentials
-s3_client = boto3.client(
+"""s3_client = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_KEY,
@@ -33,7 +33,7 @@ cloudwatch_client = boto3.client(
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_KEY,
     aws_session_token=AWS_SESSION_TOKEN,
-)
+)"""
 # Initialize AWS Clients
 s3_client = boto3.client("s3")
 sns_client = boto3.client("sns")
